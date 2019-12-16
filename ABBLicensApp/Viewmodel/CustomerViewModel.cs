176569d18af1ee579d088bd705061708eb09878c
Ronnie Damsgaard <Ronnie1983@me.com>
2019-12-16 10:50:47 +0100

    List at customer in progress

--- ABBLicensApp/Viewmodel/CustomerViewModel.cs
+++ ABBLicensApp/Viewmodel/CustomerViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 using Windows.UI.Composition.Interactions;
@@ -14,6 +16,8 @@ namespace ABBLicensApp.Viewmodel
 {
     public class CustomerViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<string> _activeLicenses = new ObservableCollection<string>();
+        private ObservableCollection<Licens> _filteredLicenses = new ObservableCollection<Licens>();
         private string _newNote;
         private string _selectedNote;
         private Customer _customer;
@@ -25,8 +29,8 @@ namespace ABBLicensApp.Viewmodel
             AddBtn = new RelayCommand(AddComment);
             DeleteNote = new RelayCommand(DeleteNoteBtn);
             Customer = Shared.SelectedCustomer;
-            Customer.Notes.Add("Hej");
-            Customer.Notes.Add("test");
+            //Customer.Notes.Add("Hej");
+            //Customer.Notes.Add("test");
         }
 
 
@@ -35,6 +39,46 @@ namespace ABBLicensApp.Viewmodel
         private void DeleteNoteBtn()
         {
             Customer.Notes.Remove(SelectedNote);
+        }
+
+        //public ObservableCollection<Licens> FilteredLicenses
+        //{
+        //    get
+        //    {
+        //        foreach (var p in Shared.Products)
+        //        {
+                    
+        //        }
+        //    }
+        //    set
+        //    {
+        //        if (Equals(value, _filteredLicenses)) return;
+        //        _filteredLicenses = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+
+        public ObservableCollection<string> ActiveLicenses
+        {
+            get
+            {
+                foreach (Product p in Shared.Products)
+                {
+                    if (p.Customer.CompanyName == Customer.CompanyName && !_activeLicenses.Contains(p.Supplier.Name))
+                    {
+                        _activeLicenses.Add(p.Supplier.Name);
+                    }
+                        
+                }
+
+                return _activeLicenses;
+            }
+            set
+            {
+                if (Equals(value, _activeLicenses)) return;
+                _activeLicenses = value;
+                OnPropertyChanged();
+            }
         }
 
         public string SelectedNote
